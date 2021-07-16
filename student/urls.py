13d2a90e333c088731610ef88521b29296665996
Utkarsh-TG <utkarshtyagi1312@gmail.com>
2021-07-16 18:35:36 +0530

    updated urls get assignment data from backend

--- student/urls.py
+++ student/urls.py
@@ -3,6 +3,7 @@ from student import views
 
 urlpatterns = [
     path('dashboard/', views.studentDashboard),
+    path('get_assignment/', views.studentGetExam),
     path('exam_submit/', views.studentExamSubmit),
     path('exam_block/', views.studentExamBlock),
     path('exam_warn/', views.studentExamWarn),
