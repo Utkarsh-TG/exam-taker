5a43b8850190ff4dd255cb954558ffc83431b414
Utkarsh-TG <utkarshtyagi1312@gmail.com>
2021-07-21 00:44:27 +0530

    important frontend resources fixed bugs

--- static/js/create-new-assignment.js
+++ static/js/create-new-assignment.js
@@ -9,42 +9,20 @@ firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 var storage = firebase.storage();
 
-//storage ref
-
-$('#assignment-form-submit').on('click', (e) => {
-    console.log('aaa')
-    loadWindow('mid', 2000)
-    file_URL = []
-    storageRef = storage.ref('Assignments').child(currentClass).child($('#assignment-title-input').val());
-    alert(storageRef)
-    // uploading file this storage ref
-    file = document.getElementById("assignment-file-input").files[0];
-    // uploading file this storage ref
-    thisref = storageRef.child(file.name).put(file);
-    thisref.on('state_changed',function(snapshot) {
-        console.log('Done');
-    }, function(error) {
-        console.log('Error',error);
-    }, function() {thisref.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-            file_URL = downloadURL
-            test()
-        });
-    });
-});
-
-function test(){
+const postAssignment = (questions) => {
     today = new Date().toISOString().slice(0, 10);
     _class = currentClass
     $.ajax({
         type: 'POST',
         url: '/teacher/exam_create/',
         data:{
+            type: 'mcq',
             class: _class,
             title: $('#assignment-title-input').val(),
             time: $('#assignment-time-input').val(),
             description: $('#assignment-description-input').val(),
             date: today,
-            file: file_URL,
+            file: questions,
             csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
         },success :function(){
             fetchAssignments();
@@ -54,5 +32,53 @@ function test(){
             $('#update-assignment').css({'display':'none'});
         }
     });
-    $('#create-assignment').trigger("reset"); 
 }
+
+$('#assignment-form-submit').on('click', (e) => {
+    loadWindow('mid', 2000)
+    today = new Date().toISOString().slice(0, 10);
+    _class = currentClass
+    if(paperType == 'long'){
+        $.ajax({
+            type: 'POST',
+            url: '/teacher/exam_create/',
+            data:{
+                type: 'long',
+                class: _class,
+                title: $('#assignment-title-input').val(),
+                time: $('#assignment-time-input').val(),
+                description: $('#assignment-description-input').val(),
+                date: today,
+                file: $('#assignment-file-input').val(),
+                csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
+            },success :function(){
+                fetchAssignments();
+                $('.create-task-wrapper').css({'display':'none'});
+                $('.assignments-container').css({'display':'block'});
+                $('#create-assignment').css({'display':'none'});
+                $('#update-assignment').css({'display':'none'});
+            }
+        });
+    }
+    if(paperType == 'mcq'){
+        let questions = []
+        questionList = document.getElementsByClassName('question-wrapper')
+        for(let j=0;j<questionList.length;j++){
+            tempData = {}
+            q = questionList[j].children[0].value
+            o1 = questionList[j].children[1].value
+            o2 = questionList[j].children[2].value
+            o3 = questionList[j].children[3].value
+            o4 = questionList[j].children[4].value
+            c = questionList[j].children[5].value
+            optionsList = [o1,o2,o3,o4]
+            tempData.question = q
+            tempData.options = optionsList
+            tempData.correct = c.toUpperCase()
+            questions.push(tempData)
+        }
+        postAssignment(JSON.stringify(questions))
+    }   
+    $('#create-assignment').trigger("reset"); 
+});
+    
