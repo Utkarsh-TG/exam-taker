7c3461109af0937cb337bc9f21f36ced9a08feb8
Utkarsh-TG <utkarshtyagi1312@gmail.com>
2021-07-22 20:51:47 +0530

    updated settings.py

--- Exam/settings.py
+++ Exam/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', '.r.appspot.com']
 
 INSTALLED_APPS = [
     'main',
