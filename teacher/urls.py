a2c08bf5b448d9e44be13c9854a5597a947de58b
Utkarsh-TG <utkarshtyagi1312@gmail.com>
2021-07-29 00:57:42 +0530

    firebase authorize teacher

--- teacher/urls.py
+++ teacher/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('exam_student_unblock/', views.examUnblockStudent),
     path('exam_result/', views.teacherExamResult),
     path('exam_results_return/', views.teacherExamResultReturn),
+    path('auth_detail/', views.authData)
 ]
